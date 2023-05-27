edb30f765f3e9c544c980936bcd8185b7b0a4112
Indraprasath <indraprasathmanickavel@gmail.com>
2023-05-27 16:42:20 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {RouterModule, Routes } from '@angular/router'; // added line
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  { path: 'contact', component: ContactComponent },
+  { path: 'home', component: HomeComponent  }      // added line
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes) //added line
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
